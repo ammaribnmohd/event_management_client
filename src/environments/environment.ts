4e2b08a5394e024258f5cbdd13c23ea64d178b78
Ammar Mohammad <ammaribnmohammad@gmail.com>
2026-03-30 10:09:32 +0600

    Update API base URL to correct production server

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://event-management-server-pi-weld.vercel.app/api',
+  apiBaseUrl: 'https://event-management-server-api.vercel.app/api',
 };
